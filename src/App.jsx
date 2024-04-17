e2dee911573ec3046d5d0c86964a025793f9897d
ann5306 <annkiunga@gmail.com>
2024-04-17 16:18:57 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+
+const App = () => {
+  return (
+    <div className='text-3xl font-bold underline '>
+      Entertainment web app
+    </div>
+  )
+}
+
+export default App
