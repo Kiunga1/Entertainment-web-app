80da648f1c2e927888e8dee6e0fba0100378e224
ann5306 <annkiunga@gmail.com>
2024-04-18 08:56:51 +0300

    Created SearchBar Component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { IoSearch } from 'react-icons/io5'
+
+const SearchBar = () => {
+  return (
+    <div className='px-3 py-2'>
+      <div className='flex gap-2 items-center rounded-md bg-primaryDark px-4 py-2'>
+        <div className='text-white '>
+            <IoSearch />
+        </div>
+        <input 
+            type="text" 
+            placeholder="Search for a movie or Tv series"
+            className='p-2 border-0 outline-none w-full placeholder:text-primaryGray text-white placeholder:text-xs placeholder-opacity-50 bg-primaryDark'
+        />
+        </div>
+    </div>
+  )
+}
+
+export default SearchBar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Header from '../components/Header'
+import SearchBar from '../components/SearchBar'
 
 const Home = () => {
   return (
-    <div>
+    <div className='lg:flex'>
       <Header/>
+      <div>
+        <SearchBar/>
+      </div>
+      
     </div>
   )
 }
